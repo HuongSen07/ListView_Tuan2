dbcefe69819f254fbdb82e6aacc07df44cf69101
Huong Sen <huongsen418@gmail.com>
2024-09-22 17:09:22 +0700

    Add project files.

---
+++ ListView_Tuan2/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ListView_Tuan2
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewSV = new System.Windows.Forms.ListView();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFistName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.FirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.LastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Phone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listViewSV
+            // 
+            this.listViewSV.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.FirstName,
+            this.LastName,
+            this.Phone});
+            this.listViewSV.FullRowSelect = true;
+            this.listViewSV.GridLines = true;
+            this.listViewSV.HideSelection = false;
+            this.listViewSV.Location = new System.Drawing.Point(12, 12);
+            this.listViewSV.MultiSelect = false;
+            this.listViewSV.Name = "listViewSV";
+            this.listViewSV.Size = new System.Drawing.Size(344, 244);
+            this.listViewSV.TabIndex = 0;
+            this.listViewSV.UseCompatibleStateImageBehavior = false;
+            this.listViewSV.View = System.Windows.Forms.View.Details;
+            this.listViewSV.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(377, 31);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(69, 16);
+            this.lblFirstName.TabIndex = 1;
+            this.lblFirstName.Text = "FirstName";
+            this.lblFirstName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(377, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LastName";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(377, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phone";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtFistName
+            // 
+            this.txtFistName.Location = new System.Drawing.Point(473, 25);
+            this.txtFistName.Name = "txtFistName";
+            this.txtFistName.Size = new System.Drawing.Size(178, 22);
+            this.txtFistName.TabIndex = 2;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(473, 125);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(178, 22);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(473, 215);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(178, 22);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(194, 332);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(77, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(326, 332);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(72, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(446, 332);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(65, 23);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // FirstName
+            // 
+            this.FirstName.Text = "FirstName";
+            this.FirstName.Width = 152;
+            // 
+            // LastName
+            // 
+            this.LastName.Text = "LastName";
+            this.LastName.Width = 74;
+            // 
+            // Phone
+            // 
+            this.Phone.Text = "Phone";
+            this.Phone.Width = 116;
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 413);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFistName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.listViewSV);
+            this.Name = "frmDangNhap";
+            this.Text = "QLSV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewSV;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFistName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.ColumnHeader FirstName;
+        private System.Windows.Forms.ColumnHeader LastName;
+        private System.Windows.Forms.ColumnHeader Phone;
+    }
+}
+
---
+++ ListView_Tuan2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListView_Tuan2
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ListViewItem lvi = new ListViewItem(txtFistName.Text);
+            lvi.SubItems.Add(txtLastName.Text);
+            lvi.SubItems.Add(txtPhone.Text);
+            listViewSV.Items.Add(lvi);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listViewSV.SelectedItems.Count > 0)
+            {
+                listViewSV.Items.Remove(listViewSV.SelectedItems[0]);
+            }
+            else
+            {
+                MessageBox.Show("Chon thanh phan de xoa!");
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (listViewSV.SelectedItems.Count > 0)
+            {
+                ListViewItem lvi = listViewSV.SelectedItems[0];
+                lvi.SubItems[0].Text = txtFistName.Text;
+                lvi.SubItems[1].Text = txtLastName.Text;
+                lvi.SubItems[2].Text = txtPhone.Text;
+            }
+            else
+            {
+                MessageBox.Show("Chon thanh phan de chinh sua!");
+            }
+        }
+    }
+}
